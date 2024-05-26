45b43376e4761dfe264c4bd1cd5b680cda9c42e1
karl5252 <karl.michalik@gmail.com>
2024-05-26 15:00:15 +0200

    chore: Update Leaderboard import path in leaderboard.js

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const Leaderboard = require(path.join(__dirname, '../models/Leaderboard')); //const Leaderboard = require('../models/Leaderboard');
 
 // GET leaderboard
