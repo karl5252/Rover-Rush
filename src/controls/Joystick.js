49a78815b7d9f8df8b08cb5174ba04a4e347a447
karl5252 <karl.michalik@gmail.com>
2024-06-02 13:15:21 +0200

    Normalize joystick angles and force values for proper player movement

--- src/controls/Joystick.js
+++ src/controls/Joystick.js
@@ -22,6 +22,24 @@ class Joystick {
     const deadzone = 0.2; // Deadzone threshold
     const force = this.joystick.force;
     const angle = this.joystick.angle;
+    // need to normalize the angles and force values to get the proper movement
+    // for now it is hard to get it right
+    // 45 degrees is joystick active but the player is not moving
+    // +16 degrees is the movement right - bit odd
+    // -178 degrees is the movement left
+    // -90 degrees is the movement up
+    // 90 degrees is the movement down
+    // Normalize force to be between 0 and 1
+    // Normalize angle to be between -180 and 180
+    while (angle > 180) {
+      angle -= 360;
+    }
+    while (angle < -180) {
+      angle += 360;
+    }
+    console.log('force:', force, 'angle:', angle); //check the angles and force values to adjust properly
+
+    const onFloor = this.player.body.onFloor();
 
     if (force > deadzone) {
       const cappedForce = Math.min(force, 1); // Cap the force to 1
@@ -32,16 +50,21 @@ class Joystick {
 
       if (angle >= -45 && angle <= 45) {
         // Right
+        this.player.setFlipX(false);
         velocityX = this.player.playerSpeed * cappedForce;
-      } else if (angle > 45 && angle < 135) {
+      } else if (angle > 58 && angle < 135 && onFloor) {
         // Down
-        velocityY = this.player.playerSpeed * cappedForce;
+        //velocityY = this.player.playerSpeed * cappedForce;
+        this.player.playerPhaseInOut();
+
       } else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
         // Left
+        this.player.setFlipX(true);
         velocityX = -this.player.playerSpeed * cappedForce;
-      } else if (angle >= -135 && angle < -45) {
+      } else if (angle < -70 && onFloor) {
         // Up
-        velocityY = -this.player.playerSpeed * cappedForce;
+        velocityY = -this.player.playerSpeed * 2.1;
+        console.log('jump');
       }
 
       this.player.setVelocityX(velocityX);
